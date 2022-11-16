a2546678097c031cd783b252545b4d7ff66f51b5
bpmurray77 <bpmurray77@gmail.com>
2022-11-16 16:48:07 +0000

    early use state

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [selectedColor, setColor] = useState("red");
+
 	return (
 		<div id="trafficTop">
 			<div id="container">
-				<div className="red light"></div>
-				<div className="yellow light"></div>
-				<div className="green light"></div>
+				<div class={"red light"+((selectedColor === "red") ? "selected" : "" )} onClick={() => setColor('red')}></div>
+				<div class={"yellow light"+((selectedColor === "yellow") ? "selected" : "" )} onClick={() => setColor('yellow')}></div>
+				<div class={"green light"+((selectedColor === "green") ? "selected" : "" )} onClick={() => setColor('green')}></div>
 			</div>
 		</div>
 	);
